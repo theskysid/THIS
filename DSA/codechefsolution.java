dae15872bcb0c35bcb5686a76a360d971c118716
theskysid <23x10sid@gmail.com>
2024-12-18 23:16:49 +0530

    Leetcode DSA progress

---
+++ DSA/codechefsolution.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class codechefsolution
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        while (true){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        if (m>=(n+1)/2){
+            System.out.println("NEWBIE");
+        }else{
+            System.out.println("PRO");
+        }
+    }}
+}
