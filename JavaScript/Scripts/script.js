1a0c1dddf94f058214b80a81ef2af9218f47c0e3
Siddhant Rastogi <23x10sid@gmail.com>
2024-12-31 01:23:41 +0530

    JavaScript added

---
+++ JavaScript/Scripts/script.js
@@ -0,0 +1,7 @@
+let cartquantity = 0;
+let string1 = "text1";
+let string2 = "text2";
+let result = string1>string2;
+document.querySelector('#testing').innerText= result;
+/*console.log('Hello Friends')
+alert('I am inside the HTMl page')*/
