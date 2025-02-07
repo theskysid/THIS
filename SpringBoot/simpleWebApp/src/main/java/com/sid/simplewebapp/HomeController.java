f5fb38d27232f97a5780503c35fcb686f5dbc809
theskysid <23x10sid@gmail.com>
2025-02-07 17:10:55 +0530

    Controller and RestController

---
+++ SpringBoot/simpleWebApp/src/main/java/com/sid/simplewebapp/HomeController.java
@@ -0,0 +1,22 @@
+package com.sid.simplewebapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @RequestMapping("/")
+//    @ResponseBody
+    public String greet(){
+        return "Welcome to Simple Web App";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "about section body called here";
+    }
+}
---
+++ SpringBoot/simpleWebApp/src/main/java/com/sid/simplewebapp/LoginController.java
@@ -0,0 +1,13 @@
+package com.sid.simplewebapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "Login page...";
+    }
+}
