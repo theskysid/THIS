af6bfed0087a8d10e99ef62a5892d84c1f889efe
theskysid <23x10sid@gmail.com>
2025-03-25 19:37:31 +0530

    todo functionality completed

--- JavaScript/todolist/script.js
+++ JavaScript/todolist/script.js
@@ -1,1 +1,34 @@
-console.log("Siddhant");
+let todoList = [];
+
+function addTodo() {
+   let inputElement = document.querySelector(`#todo-input`);
+
+   let dateElement = document.querySelector('#todo-date');
+
+   let todoItem = inputElement.value;
+   let todoDate = dateElement.value;
+
+   todoList.push({item: todoItem,dueDate: todoDate});
+
+   inputElement.value='';
+   dateElement.value='';
+
+   displayItems();
+}
+
+function displayItems(){
+   let containerElement = document.querySelector('.todo-container');
+
+   let newHtml = '';
+
+   for(let i = 0; i< todoList.length; i++){
+      let {item, dueDate} = todoList[i];
+
+      newHtml += `
+            <span>${item}</span>
+            <span>${dueDate}</span>
+            <button onclick = "todoList.splice(${i},1); displayItems();">Delete</button>
+         `  
+   }
+   containerElement.innerHTML = newHtml;
+}
