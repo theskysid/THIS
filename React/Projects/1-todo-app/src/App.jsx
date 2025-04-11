4dc03da564898a1018541d1a3020a1eb04022571
theskysid <23x10sid@gmail.com>
2025-04-11 13:35:38 +0530

    updated

---
+++ React/Projects/1-todo-app/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import TodoItem from "./components/TodoItem";
+
+
+function App() {
+  return (
+    <center class="todo-container">
+      <AppName />
+      <AddTodo />
+      <TodoItem />
+    </center>
+  );
+}
+
+export default App;
