e98646017d8afb9f54d2f76a61a30178e2568308
theskysid <23x10sid@gmail.com>
2025-04-12 18:26:18 +0530

    AppName.module.css file created

--- React/Projects/1-todo-app/src/components/AppName.jsx
+++ React/Projects/1-todo-app/src/components/AppName.jsx
@@ -1,5 +1,7 @@
+import styles from "./AppName.module.css";
+
 function AppName() {
-   return <h1>Todo React App</h1>
+   return <h1 className={styles.todoHeading}>Todo React App</h1>
 }
 
 export default AppName;
