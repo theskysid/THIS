15d22379e364fd5aae97afd6bd56543950181801
theskysid <23x10sid@gmail.com>
2025-04-12 16:47:31 +0530

    dsa done today

--- DSA/Solution.java
+++ DSA/Solution.java
@@ -1,33 +1,7 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n; // To handle cases where k > n
-
-        // Step 1: Reverse the entire array
-        reverse(nums, 0, n - 1);
-
-        // Step 2: Reverse the first k elements
-        reverse(nums, 0, k - 1);
-
-        // Step 3: Reverse the remaining elements
-        reverse(nums, k, n - 1);
-    }
-    private static void reverse(int[] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
-        solution.rotate(nums, 3);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-        }
+        System.out.println("hi");
     }
 }
--- DSA/testing.java
+++ DSA/testing.java
@@ -1,2 +1,12 @@
+import java.util.Stack;
+
 public class testing {
+    public static void main(String[] args) {
+        String s = "abcd";
+        Stack<Integer> st = new Stack<>();
+        char[] sarray = s.toCharArray();
+        for(char c : sarray){
+            System.out.println(sarray[c]);
+        }
+    }
 }
