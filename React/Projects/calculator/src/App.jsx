2a3907de916b0e881acfdd949189aa502ba1e517
theskysid <23x10sid@gmail.com>
2025-04-27 16:06:02 +0530

    calculator completed

--- React/Projects/calculator/src/App.jsx
+++ React/Projects/calculator/src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+import ButtonsContainer from "./components/ButtonsContainer";
+import styles from "./App.module.css";
+import { useState } from "react";
+import Display from "./components/Display";
 
 function App() {
-  
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClickfunction = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
 
   return (
-    <>
-
-    </>
-  )
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClickfunction}></ButtonsContainer>
+    </div>
+  );
 }
 
-export default App
+export default App;
