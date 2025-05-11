f67a5ee0fa850e14bd96197ac6520e79bd9c1612
theskysid <23x10sid@gmail.com>
2025-05-11 08:47:42 +0530

    useState to useRef and form used ....  overall performance increased

--- React/Projects/1-todo-app/src/components/AddTodo.jsx
+++ React/Projects/1-todo-app/src/components/AddTodo.jsx
@@ -1,47 +1,66 @@
-import { useState } from "react";
+import { useRef } from "react";
+//import { useState } from "react";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState();
-  const [dueDate, setDueDate] = useState();
 
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
+  //first we were re painting the whole page when a change occurs now we are using useref to just modify the values 
+  // const [todoName, setTodoName] = useState();
+  // const [dueDate, setDueDate] = useState();
 
-  const handleAddButtonClicked = () => {
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  // };
+
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  // };
+
+  const handleAddButtonClicked = (e) => {
+    e.preventDefault();
+
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+
     onNewItem(todoName, dueDate);
-    setDueDate("");
-    setTodoName("");
+
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
+
+    // setDueDate("");
+    // setTodoName("");
   };
 
   return (
     <div className="container text-center">
-      <div className="row kg-row">
+      <form className="row kg-row" onSubmit={handleAddButtonClicked}>
         <div className="col-6">
           <input
             type="text"
             placeholder="Enter Todo Here"
-            value={todoName}
-            onChange={handleNameChange}
+            ref={todoNameElement}
+            // value={todoName}
+            //onChange={handleNameChange}
           />
         </div>
         <div className="col-4">
-          <input type="date" value={dueDate} onChange={handleDateChange} />
+          <input type="date" 
+          ref={dueDateElement}
+          //value={dueDate} 
+          //onChange={handleDateChange} 
+           />
         </div>
         <div className="col-2">
           <button
-            type="button"
+            type="submit"
             className="btn btn-success kg-button"
-            onClick={handleAddButtonClicked}
           >
             Add
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
