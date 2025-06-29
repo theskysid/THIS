4818225ad16f19b24e45647c1371ea6a217cb3cc
theskysid <23x10sid@gmail.com>
2025-06-29 15:50:05 +0530

    Setter injection in Asus

--- SpringBoot/LaptopDealer/src/main/java/com/theskysid/laptopdealer/Asus.java
+++ SpringBoot/LaptopDealer/src/main/java/com/theskysid/laptopdealer/Asus.java
@@ -3,10 +3,16 @@ package com.theskysid.laptopdealer;
 public class Asus implements Brand {
     Processor processor;
 
-    //constructer injection
-    public Asus(Processor processor) {
+//    //constructer injection
+//    public Asus(Processor processor) {
+//        this.processor = processor;
+//    }
+
+    //setter injection
+    public void setProcessor(Processor processor) {
         this.processor = processor;
     }
+
     public void showDetails() {
         System.out.println("You have selected Asus laptop" +   processor.showProcessorDetails());
     }
--- SpringBoot/LaptopDealer/src/main/java/com/theskysid/laptopdealer/LaptopDealerApplication.java
+++ SpringBoot/LaptopDealer/src/main/java/com/theskysid/laptopdealer/LaptopDealerApplication.java
@@ -9,67 +9,67 @@ import java.util.Scanner;
 //@SpringBootApplication
 public class LaptopDealerApplication {
 
-	public static void main(String[] args) {
-		//SpringApplication.run(LaptopDealerApplication.class, args);
+    public static void main(String[] args) {
+        //SpringApplication.run(LaptopDealerApplication.class, args);
 
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-		Scanner sc =  new Scanner(System.in);
+        Scanner sc =  new Scanner(System.in);
 
-		System.out.println("Choose the brand you want to buy\n1. HP\n2. Asus\n3. Dell");
-		int choice = sc.nextInt();
+        System.out.println("Choose the brand you want to buy\n1. HP\n2. Asus\n3. Dell");
+        int choice = sc.nextInt();
 
-		System.out.println("Choose the processor for your laptop\n1. i3\n2. i5\n3. i7");
-		int processor = sc.nextInt();
+        System.out.println("Choose the processor for your laptop\n1. i3\n2. i5\n3. i7");
+        int processor = sc.nextInt();
 
-		String beanId = "";
+        String beanId = "";
 
-		switch (choice) {
-			case 1:
-				switch (processor) {
-					case 1:
-						beanId = "hpwithi3";
-						break;
-					case 2:
-						beanId = "hpwithi5";
-						break;
-					case 3:
-						beanId = "hpwithi7";
-						break;
-				}
-				break;
-			case 2:
-				switch (processor) {
-					case 1:
-						beanId = "Asuswithi3";
-						break;
-						case 2:
-							beanId = "Asuswithi5";
-							break;
-							case 3:
-								beanId = "Asuswithi7";
-								break;
+        switch (choice) {
+            case 1:
+                switch (processor) {
+                    case 1:
+                        beanId = "hpwithi3";
+                        break;
+                    case 2:
+                        beanId = "hpwithi5";
+                        break;
+                    case 3:
+                        beanId = "hpwithi7";
+                        break;
+                }
+                break;
+            case 2:
+                switch (processor) {
+                    case 1:
+                        beanId = "Asuswithi3";
+                        break;
+                    case 2:
+                        beanId = "Asuswithi5";
+                        break;
+                    case 3:
+                        beanId = "Asuswithi7";
+                        break;
+                }
+                break;
+            case 3:
+                switch (processor) {
+                    case 1:
+                        beanId = "dellwithi3";
+                        break;
+                    case 2:
+                        beanId = "dellwithi5";
+                        break;
+                    case 3:
+                        beanId = "dellwithi7";
+                        break;
 
-				}
-				break;
-				case 3:
-					switch (processor) {
-						case 1:
-							beanId = "dellwithi3";
-							break;
-						case 2:
-							beanId = "dellwithi5";
-							break;
-							case 3:
-								beanId = "dellwithi7";
-								break;
+                }
+                break;
+        }
 
-				}
-				break;
-		}
-		Brand laptop = (Brand) context.getBean(beanId);
-		laptop.showDetails();
-	}
+        Brand laptop = (Brand) context.getBean(beanId);
+        laptop.showDetails();
+    }
 
 }
