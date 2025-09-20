854d4e659a5a360a1b1f7c424672b3b714ec78be
theskysid <23x10sid@gmail.com>
2025-09-20 19:33:56 +0530

    Single elemnent in a sorted array

---
+++ DSA/Leetcode/BinarySearch/M_singleelementinasortedarray_540.java
@@ -0,0 +1,25 @@
+package Leetcode.BinarySearch;
+
+public class M_singleelementinasortedarray_540 {
+    public static int singleNonDuplicate(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(mid%2==1){
+                mid--;
+            }
+            if(nums[mid]==nums[mid+1]){
+                start=mid+2;
+            }else{
+                end=mid;
+            }
+        }
+        return nums[start];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(arr));
+    }
+}
---
+++ DSA/Leetcode/TwoPointer/M_stringcompression_443.java
@@ -0,0 +1,4 @@
+package Leetcode.TwoPointer;
+
+public class M_stringcompression_443 {
+}
